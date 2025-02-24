54f0b513fbe64c66be88c826726fdbafcde4222d
Michal Huszcza <michal@MacBookAir.lan>
2025-02-24 18:04:38 +0100

    First Xcode SwiftUI project setup

---
+++ secondShore/secondShore/secondShoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondShoreApp.swift
+//  secondShore
+//
+//  Created by Michal Huszcza on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct secondShoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
